8ea38eac878c19b88b9efce57747a4ff5f323d69
Samking213 <babuinsa184@gmail.com>
2026-02-22 16:28:25 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,26 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
+import { useState } from 'react';
+import Home from './components/Home';
+import Reader from './components/Reader';
+
+export default function App() {
+  const [currentPara, setCurrentPara] = useState<number | null>(null);
+
+  return (
+    <>
+      {currentPara ? (
+        <Reader 
+          paraNum={currentPara} 
+          onBack={() => setCurrentPara(null)} 
+          onNextPara={() => setCurrentPara(currentPara + 1)}
+        />
+      ) : (
+        <Home onSelectPara={setCurrentPara} />
+      )}
+    </>
+  );
+}
